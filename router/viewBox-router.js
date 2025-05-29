17176c9e8b0ff278b9992fdde66f00865f618bec
Darkiane <darkianelima13@gmail.com>
2025-05-29 18:09:35 -0300

    Colocando as pastas da api em uma única pasta

--- router/viewBox-router.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import getViewBox from '../controller/viewBox-controller.js';
-
-router.get('/:estado', getViewBox);
-
-export default router;
